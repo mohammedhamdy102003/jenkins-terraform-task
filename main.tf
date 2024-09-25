25c3f153a38b956c7bac9192df6f5ae983a8175b
mohammed hamdy <mohammedhamdy102003@gmail.com>
2024-09-25 20:42:17 +0300

    message

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+variable "number_of_containers" {
+  type    = number
+  default = 1
+}
+
+variable "image_name" {
+  type    = string
+  default = "nginx"
+}
+
+variable "image_version" {
+  type    = string
+  default = "latest"
+}
+
+resource "docker_image" "nginx" {
+  name = "${var.image_name}:${var.image_version}"
+}
+
+resource "docker_container" "nginx_containers" {
+  count = var.number_of_containers
+  image = docker_image.nginx.latest
+  name  = "nginx-${count.index + 1}"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
